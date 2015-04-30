08faca3c98d71fd77589b1d2466ba3c77919815a
gentlekevin <gentlekevin@126.com>
2015-04-30 18:20:06 +0800

    init

---
+++ src/main/java/com/btict/rest/account/RestRegisterController.java
@@ -0,0 +1,29 @@
+package com.btict.rest.account;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springside.modules.web.MediaTypes;
+
+import com.btict.entity.User;
+import com.btict.service.account.AccountService;
+
+@RestController
+@RequestMapping(value = "/rest/register")
+public class RestRegisterController {
+	@Autowired
+	private AccountService accountService;
+	
+	@RequestMapping(method = RequestMethod.POST,produces = MediaTypes.JSON_UTF_8)
+	
+	public User register(@Valid User user) {
+	
+		return accountService.registerRestUser(user);
+
+		
+	
+	}
+}
